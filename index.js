1bf8412d504cfa247d6e8b7f0310a23998babe5d
Robbo <robbo.clancy@gmail.com>
2018-03-09 17:44:13 -0300

    fixed pathing issues with node_modules, still most likely doing this wrong

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var path = require('path');
 var Funnel = require('broccoli-funnel');
 var mergeTrees = require('broccoli-merge-trees');
+var resolve = require('resolve');
 
 module.exports = {
   name: 'ember-autolinkify',
@@ -10,14 +11,21 @@ module.exports = {
   included: function(app) {
     this._super.included.apply(this, arguments);
 
+    // see: https://github.com/ember-cli/ember-cli/issues/3718
+    if (typeof app.import !== 'function' && app.app) {
+      app = app.app;
+    }
+
+    this.app = app;
+
     this.import('vendor/autolinker/Autolinker.min.js');
     this.import('vendor/shims/autolinker.js');
   },
 
   treeForVendor(tree) {
-    let autolinkerPath = path.join(this.app.project.nodeModulesPath, 'autolinker/dist');
+    let autolinkerPath = resolve.sync('autolinker', { basedir: this.app.project.root });
 
-    let copiedTree = new Funnel(autolinkerPath, {
+    let copiedTree = new Funnel(autolinkerPath.replace('/Autolinker.js', ''), {
       destDir: 'autolinker',
       files: ['Autolinker.min.js']
     });
