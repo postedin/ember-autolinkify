1bbbaf4fb8d2c240e69bdac58afc3e7c9bf871ea
Robbo <robbo.clancy@gmail.com>
2018-04-24 19:15:19 -0300

    helper to be htmlSafe automatically

--- addon/helpers/linkify.js
+++ addon/helpers/linkify.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
 import Autolinker from 'autolinker';
+import { htmlSafe } from '@ember/string';
 
 export function linkify(text, options) {
   return Autolinker.link(text, options);
 }
 
-export default Ember.Helper.helper(function ([text], options) { return linkify(text, options); });
+export default Ember.Helper.helper(function ([text], options) { return htmlSafe(linkify(text, options)); });
